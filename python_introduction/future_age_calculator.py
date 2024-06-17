a10401d1255de1dfe6acdfbf6d2d5eaf1f8174b3
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-06-17 09:55:38 +0100

    future age

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+"""receive user input and calculate the user’s age in a future year."""
+
+age = int(input("How old are you?: "))
+future_year = 2050
+current_year = 2023
+
+print(f'In {future_year}, you will be {age + (future_year - current_year)} years old.')
