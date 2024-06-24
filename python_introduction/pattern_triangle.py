a2cfe4bce5e9d6ae73af871229b03eb69f83b5f7
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-06-24 23:07:52 +0100

    Triangle Pattern

---
+++ python_introduction/pattern_triangle.py
@@ -0,0 +1,22 @@
+"""Triangle pattern"""
+n = int(input("Enter the number of rows: "))
+i = 0
+j = 0
+k = n
+
+while i < n:
+    print(" " * (k - 1), end="")
+    print("*" * (j + 1), end="")
+    print(end="\n")
+    i += 1
+    j += 2
+    k -= 1
+
+"""
+Output:
+*
+**
+***
+****
+*****
+"""
