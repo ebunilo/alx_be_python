c9f769c7d02df4eece6e72bd58bc85a7338de745
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-07-07 13:28:36 +0100

    remove spaces

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 """defines functions to convert temperatures between Celsius and Fahrenheit"""
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     """
