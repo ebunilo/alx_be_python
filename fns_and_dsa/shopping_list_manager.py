c0eb631d80c4034ab3c6291053e7edf3d161c4d8
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-07-07 13:43:41 +0100

    update prompts

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,11 +14,11 @@ def main():
         choice = input("Enter your choice: ")
         if choice == '1':
             # Prompt for and add an item
-            item = str(input("Enter the name of item to add: "))            
+            item = str(input("Enter the item to add: "))            
             shopping_list.append(item)
         elif choice == '2':
             # Prompt for and remove an item
-            item = str(input("Enter the name of item to remove: "))
+            item = str(input("Enter the item to remove: "))
             if item in shopping_list:
                 shopping_list.remove(item)
             else:
