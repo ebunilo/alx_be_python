459d0bb666d777785bab0bb7c908e3871b62053e
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-07-14 12:25:24 +0100

    add comments in return statement

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -7,4 +7,4 @@ def safe_divide(numerator, denominator):
         return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
-    return result
+    return f"The result of the division is {result}"
