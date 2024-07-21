6c2bdc1b11a42b4bac1f86a132ea66655780d9ab
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-07-21 12:12:07 +0100

    update variable names

--- oop/library_system.py
+++ oop/library_system.py
@@ -8,21 +8,21 @@ class Book:
 
 
 class EBook(Book):
-    def __init__(self, title: str, author: str, filesize: int):
+    def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
-        self.filesize = filesize
+        self.file_size = file_size
 
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.filesize}"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
 
 
 class PrintBook(Book):
-    def __init__(self, title: str, author: str, pagecount: int):
+    def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
-        self.pagecount = pagecount
+        self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.pagecount}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
