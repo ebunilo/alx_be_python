1f19350c7097c3b51ba9a8879fa6860062718297
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-07-27 23:38:34 +0100

    add multiple records to table

---
+++ Intro_to_DB/task_6.sql
@@ -0,0 +1,9 @@
+-- Script that inserts multiple rows in the table customer
+-- The database name will be passed as an argument of the mysql command
+USE alx_book_store
+
+INSERT INTO Customers(customer_id, customer_name, email, address)
+VALUES
+    (2, 'Blessing Malik', 'bmailik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.')
