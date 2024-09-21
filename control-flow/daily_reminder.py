4cb6002faf7f8da7355d41edb69392b4cfa342e9
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-09-21 23:11:23 +0100

    remove .upper method in user input

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,10 +7,12 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-if time_bound.lower() == "yes":
-    reminder = f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!"
+if time_bound == "yes":
+    reminder = f"Reminder: '{task}' is a {
+        priority} priority task that requires immediate attention today!"
 else:
-    reminder = f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time."
+    reminder = f"Note: '{task}' is a {
+        priority} priority task. Consider completing it when you have free time."
 
 match priority:
     case "high":
