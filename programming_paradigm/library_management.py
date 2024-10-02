3b0b7cb905fd3ef00d2d7d3b5e9a597c59687dc4
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-10-02 20:06:41 +0100

    implement checkout and return_book methods in Book class

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,6 +4,21 @@ class Book:
         self.author = author
         self._is_checked_out = False
 
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+    
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False   
+
 
 class Library:
     def __init__(self):
