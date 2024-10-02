9b0c479527f985c60999d34bbe7c8640731478e3
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-10-02 14:25:14 +0100

    update the the display_balance output

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -24,4 +24,4 @@ class BankAccount:
 
     def display_balance(self):
         """Display current account balance"""
-        print(f"Current balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
